403abc2bd51d80800a17269ff24b80718ce9c57c
RWArunde <filbertrunde@gmail.com>
2016-02-09 15:17:53 -0500

    3_state samples

--- octave_hmm/gen_3_state.m
+++ octave_hmm/gen_3_state.m
@@ -1,16 +1,18 @@
 trans = [0, 0.9, 0.05, 0.05; 0, 0.8, 0.15, 0.05; 0, 0, 0.7, 0.3; 0, 0, 0, 1];
 emis = [1, 0; 0.75, 0.25; 0.5, 0.5; 0.1, 0.9];
 
-seq = zeros(60,19);
+num = 300;
 
-for c = 1:60
-    seq(c,:) = [hmmgenerate(18, trans, emis), 0];
+seq = zeros(num,11);
+
+for c = 1:num
+    seq(c,:) = [hmmgenerate(10, trans, emis), 0];
 end
 seq = seq - 1;
 
-probs = zeros(60,19);
-for c = 1:60
-    probs(c,:) = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,-1];
+probs = zeros(num,11);
+for c = 1:num
+    probs(c,:) = [0,0,0,0,0,0,0,0,0,0,-1];
 end
 
 csvwrite("observations_3state.csv", seq);
