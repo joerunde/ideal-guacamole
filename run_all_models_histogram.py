e270dc1cfa09fe4aec5c89b4473ad5622c754f21
RWArunde <filbertrunde@gmail.com>
2016-04-02 20:45:18 -0400

    What is even a tuple?

--- run_all_models_histogram.py
+++ run_all_models_histogram.py
@@ -2,36 +2,36 @@ import thread, os
 
 #thread.start_new_thread
 
-def syscall(cmd):
+def syscall(cmd, crap=None):
     os.system(cmd)
 
 
 
 #run BKT models
-thread.start_new_thread(syscall,('python run_BKT.py x_axis y_axis h_to_d d_to_h center shape spread histogram xy css descrip whole_tutor') )
-thread.start_new_thread(syscall,('python run_BKT-4.py x_axis y_axis h_to_d d_to_h center shape spread histogram xy css descrip whole_tutor') )
+thread.start_new_thread(syscall, ('python run_BKT.py x_axis y_axis h_to_d d_to_h center shape spread histogram xy css descrip whole_tutor',0) )
+thread.start_new_thread(syscall, ('python run_BKT-4.py x_axis y_axis h_to_d d_to_h center shape spread histogram xy css descrip whole_tutor',0) )
 
 
 #run basic LFKT models
-thread.start_new_thread(syscall,('python run_LFKT.py x_axis y_axis h_to_d d_to_h center shape spread histogram xy css descrip whole_tutor') )
-thread.start_new_thread(syscall,('python run_LFKT-4.py x_axis y_axis h_to_d d_to_h center shape spread histogram xy css descrip whole_tutor') )
-thread.start_new_thread(syscall,('python run_LFKT_L1.py x_axis y_axis h_to_d d_to_h center shape spread histogram xy css descrip whole_tutor') )
+thread.start_new_thread(syscall,('python run_LFKT.py x_axis y_axis h_to_d d_to_h center shape spread histogram xy css descrip whole_tutor',0) )
+thread.start_new_thread(syscall,('python run_LFKT-4.py x_axis y_axis h_to_d d_to_h center shape spread histogram xy css descrip whole_tutor',0) )
+thread.start_new_thread(syscall,('python run_LFKT_L1.py x_axis y_axis h_to_d d_to_h center shape spread histogram xy css descrip whole_tutor',0) )
 
 
 #run skill difficulty model
-thread.start_new_thread(syscall,('python run_LFKT-skills_L1.py xy css descrip whole_tutor') )
+thread.start_new_thread(syscall,('python run_LFKT-skills_L1.py xy css descrip whole_tutor',0) )
 
 
 #run transition models
-thread.start_new_thread(syscall,('python run_LFKT-trans_L1.py x_axis y_axis h_to_d d_to_h center shape spread histogram xy css descrip whole_tutor') )
-thread.start_new_thread(syscall,('python run_LFKT-trans-first_L1.py x_axis y_axis h_to_d d_to_h center shape spread histogram xy css descrip whole_tutor') )
-thread.start_new_thread(syscall,('python run_LFKT-trans-diff_L1.py x_axis y_axis h_to_d d_to_h center shape spread histogram') )
-thread.start_new_thread(syscall,('python run_LFKT-trans-diff-first_L1.py x_axis y_axis h_to_d d_to_h center shape spread histogram') )
-thread.start_new_thread(syscall,('python run_LFKT-trans-diff_L1.py xy css descrip whole_tutor') )
-thread.start_new_thread(syscall,('python run_LFKT-trans-diff-first_L1.py xy css descrip whole_tutor') )
+thread.start_new_thread(syscall,('python run_LFKT-trans_L1.py x_axis y_axis h_to_d d_to_h center shape spread histogram xy css descrip whole_tutor',0) )
+thread.start_new_thread(syscall,('python run_LFKT-trans-first_L1.py x_axis y_axis h_to_d d_to_h center shape spread histogram xy css descrip whole_tutor',0) )
+thread.start_new_thread(syscall,('python run_LFKT-trans-diff_L1.py x_axis y_axis h_to_d d_to_h center shape spread histogram',0) )
+thread.start_new_thread(syscall,('python run_LFKT-trans-diff-first_L1.py x_axis y_axis h_to_d d_to_h center shape spread histogram',0) )
+thread.start_new_thread(syscall,('python run_LFKT-trans-diff_L1.py xy css descrip whole_tutor',0) )
+thread.start_new_thread(syscall,('python run_LFKT-trans-diff-first_L1.py xy css descrip whole_tutor',0) )
 
 #run knowledge transfer models
-thread.start_new_thread(syscall,('python run_LFKT-useful_L1.py xy css descrip') )
-thread.start_new_thread(syscall,('python run_LFKT-useful-diff_L1.py xy css descrip') )
-thread.start_new_thread(syscall,('python run_LFKT-useful_L1.py whole_tutor') )
-thread.start_new_thread(syscall,('python run_LFKT-useful-diff_L1.py whole_tutor') )
+thread.start_new_thread(syscall,('python run_LFKT-useful_L1.py xy css descrip',0) )
+thread.start_new_thread(syscall,('python run_LFKT-useful-diff_L1.py xy css descrip',0) )
+thread.start_new_thread(syscall,('python run_LFKT-useful_L1.py whole_tutor',0) )
+thread.start_new_thread(syscall,('python run_LFKT-useful-diff_L1.py whole_tutor',0) )
