e8a9eb7b9776649c3a448042147b309fa1712bf1
RWArunde <filbertrunde@gmail.com>
2016-04-11 23:42:00 -0400

    no L1 param here

--- sim_KT.py
+++ sim_KT.py
@@ -6,7 +6,7 @@ def syscall(cmd, crap=None):
     os.system(cmd)
 
 for student in [10,25,50,75,100,500]:
-    thread.start_new_thread(syscall,('python fit_MLFKT_usefulness_penalty_model.py 100 1000 2 n simulated_KT2_' + str(student) + ' 0 1 L1 0.5', 0))
+    thread.start_new_thread(syscall,('python fit_MLFKT_usefulness_penalty_model.py 100 1000 2 n simulated_KT2_' + str(student) + ' 0 1 0.5', 0))
     #os.system('python fit_MLFKT_transition_model.py 100 1000 2 n simulated_KT2_' + str(student) + ' 0 1 L1')
 
 time.sleep(5)
